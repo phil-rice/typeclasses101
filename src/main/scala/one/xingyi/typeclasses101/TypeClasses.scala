8c268af50bac6080883564618152c6bdcaa97370
phil <phil.rice@validoc.org>
2021-03-10 08:48:32 +0100

    adjusted start

--- src/main/scala/one/xingyi/typeclasses101/TypeClasses.scala
+++ src/main/scala/one/xingyi/typeclasses101/TypeClasses.scala
@@ -11,6 +11,12 @@ object TypeClass {
   val maps = List(Map("a" -> 1), Map("b" -> 2), Map("c" -> 3))
   val money = List(Money(1), Money(2), Money(3))
 
+  def addInts(ts: List[Int]) = ts.foldLeft(0) { _ + _ }
+  def addDoubles(ts: List[Double]) = ts.foldLeft(0.0) { _ + _ }
+  def addStrings(ts: List[String]) = ts.foldLeft("") { _ + _ }
+  def addMaps[K, V](ts: List[Map[K, V]]) = ts.foldLeft(Map[K, V]()) { _ ++ _ }
+  def addMoney(ts: List[Money]) = ts.foldLeft(Money(0)) { _ + _ }
+
   def addAll[T](list: List[T]): T = ???
 
 

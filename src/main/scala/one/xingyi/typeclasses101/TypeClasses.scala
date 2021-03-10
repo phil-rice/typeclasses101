5ae1c265e32c6378b3734f6cd3d77e3b567aa147
phil <phil.rice@validoc.org>
2021-03-10 08:23:41 +0100

    some tests

--- src/main/scala/one/xingyi/typeclasses101/TypeClasses.scala
+++ src/main/scala/one/xingyi/typeclasses101/TypeClasses.scala
@@ -1,11 +1,15 @@
 package one.xingyi.typeclasses101
+import scala.concurrent.Future
 
-
+case class Money(amount: Int) {
+  def +(other: Money) = Money(amount + other.amount)
+}
 object TypeClass {
   val ints = List(1, 2, 3)
   val doubles = List(1.0, 2.0, 3.0)
   val strings = List("one", "two", "three")
   val maps = List(Map("a" -> 1), Map("b" -> 2), Map("c" -> 3))
+  val money = List(Money(1), Money(2), Money(3))
 
   def addAll[T](list: List[T]): T = ???
 
@@ -15,7 +19,17 @@ object TypeClass {
 object HigherOrderTypeClases {
   val list = List(1, 2, 3)
   val vector = Vector(1, 2, 3)
-  val set = set(1, 2, 3)
+  val set = Set(1, 2, 3)
+  val future = Future.successful(6)
+
+  def addAll[F[_], T](f: F[T]): F[T] = ???
+
+}
+object CombinedTypeClases {
+  val list = List(1, 2, 3)
+  val vector = Vector(1.0, 2.0, 3.0)
+  val set = Set(Money(1), Money(2), Money(3))
+  val future = Future.successful(Money(6))
 
   def addAll[F[_], T](f: F[T]): T = ???
 
---
+++ src/test/scala/one/xingyi/typeclasses101/TypeClassTest.scala
@@ -0,0 +1,43 @@
+package one.xingyi.typeclasses101
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should
+
+import scala.concurrent.Future
+
+class TypeClassTest extends AnyFlatSpec with should.Matchers {
+
+  behavior of "TypeClass"
+
+  it should "allow numbers to be added" in {
+    import TypeClass._
+    addAll(ints) shouldBe 6
+    addAll(doubles) shouldBe 6
+    addAll(strings) shouldBe "onetwothree"
+    addAll(maps) shouldBe Map("a" -> 1, "b" -> 2, "c" -> 3)
+    addAll(money) shouldBe Money(6)
+  }
+
+  behavior of "HigherOrderTypeClasses"
+
+  it should "allow us to abstract over lists/vectors/etc using applicatives" in {
+    import HigherOrderTypeClases._
+
+    addAll(list) shouldBe List(6)
+    addAll(vector) shouldBe Vector(6)
+    addAll(set) shouldBe Set(6)
+    addAll(future) shouldBe Future.successful(6)
+  }
+
+
+  behavior of "Combined"
+
+  it should "allow to have different applicatives and different types" in {
+    import CombinedTypeClases._
+
+    addAll(list) shouldBe List(6)
+    addAll(vector) shouldBe Vector(6.0)
+    addAll(set) shouldBe Set(Money(6))
+    addAll(future) shouldBe Future.successful(Money(6))
+  }
+}
